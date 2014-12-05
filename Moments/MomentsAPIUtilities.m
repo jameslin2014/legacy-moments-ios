ad1bf13e36b48694f51e88472315f3e0ecbc219f
Colton Anglin <hi@imcolton.me>
2014-12-04 23:14:49 -0600

    Updated for auth

--- Moments/MomentsAPIUtilities.m
+++ Moments/MomentsAPIUtilities.m
@@ -16,7 +16,7 @@
 // Grab all user data from Firebase with a specified username
 - (void)getAllUserDataWithUsername:(NSString *)username completion:(void (^)(NSDictionary *))data {
     [Firebase goOnline];
-    Firebase *UserPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/%@",username]];
+    Firebase *UserPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/users/%@",username]];
     [UserPath observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         NSLog(@"Success");
         data(snapshot.value);
@@ -30,7 +30,8 @@
 // Grab the phone number from Firebase with a specified username
 - (void)getUserPhoneNumberWithUsername:(NSString *)username completion:(void (^)(NSString *))data {
     [Firebase goOnline];
-    Firebase *UserPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/%@/phone_number",username]];
+    Firebase *UserPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/users/%@/phone_number",username]];
+    
     [UserPath observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         NSString *phoneData = snapshot.value;
         data(phoneData);
@@ -44,7 +45,7 @@
 // Grab a user password from Firebase with a specified username
 - (void)getUserPasswordWithUsername:(NSString *)username completion:(void (^)(NSString *))data {
     [Firebase goOnline];
-    Firebase *UserPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/%@/password",username]];
+    Firebase *UserPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/users/%@/password",username]];
     [UserPath observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         NSString *password = snapshot.value;
         data(password);
@@ -59,7 +60,7 @@
 // Grabs a user's following list from Firebase from a username.
 - (void)getUserFollowingListWithUsername:(NSString *)username completion:(void (^)(NSArray *))data {
     [Firebase goOnline];
-    Firebase *followingPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/%@/following",username]];
+    Firebase *followingPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/users/%@/following",username]];
     [followingPath observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         if (snapshot.value == [NSNull null]) {
             NSArray *empty = @[];
@@ -83,7 +84,7 @@
 // Grabs a user's followers list from Firebase from a username.
 - (void)getUserFollowersListWithUsername:(NSString *)username completion:(void (^)(NSArray *))data {
     [Firebase goOnline];
-    Firebase *followingPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/%@/followers",username]];
+    Firebase *followingPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/users/%@/followers",username]];
     [followingPath observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         if (snapshot.value == [NSNull null]) {
             NSArray *empty = @[];
@@ -110,13 +111,13 @@
 // Attempt to login with specified credentials (username/pass) and throw a boolean with the result
 - (void)loginWithUsername:(NSString *)username andPassword:(NSString *)password completion:(void (^)(BOOL))data {
     [Firebase goOnline];
-    Firebase *userPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/%@",username]];
+    Firebase *userPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/users/%@",username]];
     [userPath observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         if (snapshot.value == [NSNull null]) {
             data(false);
             [Firebase goOffline];
         } else {
-            Firebase *passPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/%@/password",username]];
+            Firebase *passPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/users/%@/password",username]];
             [passPath observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
                 NSString *pass = snapshot.value;
                 if (snapshot.value == [NSNull null]) {
@@ -147,7 +148,7 @@
 // Follow a user from a specified username and return a boolean with the follow status
 - (void)followUserWithUsername:(NSString *)followedUsername fromUsername:(NSString *)followerUsername completion:(void (^)(BOOL))data {
     [Firebase goOnline];
-    Firebase *userPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/%@/following",followerUsername]];
+    Firebase *userPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/users/%@/following",followerUsername]];
     [userPath observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         followingArray = snapshot.value;
         if (snapshot.value == [NSNull null]) {
@@ -212,7 +213,7 @@
 // Search through Firebase for a specific user. A boolean returns true if its a valid user, false if not
 - (void)searchForUsersWithUserName:(NSString *)searchString completion:(void (^)(BOOL))data {
     [Firebase goOnline];
-    Firebase *userPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/%@",searchString]];
+    Firebase *userPath = [[Firebase alloc] initWithUrl: [NSString stringWithFormat:@"https://moments-users.firebaseio.com/users/%@",searchString]];
     [userPath observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         if (snapshot.value == [NSNull null]) {
             NSLog(@"User: %@ does not exist",searchString);
