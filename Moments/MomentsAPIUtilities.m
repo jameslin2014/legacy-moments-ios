6214a1e9857d626c61d587541f1590ec85d3ac2c
Damon Jones <damon@damonjones.me>
2015-01-04 04:20:09 -0500

    Hard-code the server IP until DNS propagates

--- Moments/MomentsAPIUtilities.m
+++ Moments/MomentsAPIUtilities.m
@@ -10,7 +10,8 @@
 
 @implementation MomentsAPIUtilities
 
-static const NSString *apiEndpoint = @"http://pickmoments.io/api/users/";
+//static const NSString *apiEndpoint = @"http://pickmoments.io/api/users/";
+static const NSString *apiEndpoint = @"http://107.170.194.151/app.php/api/users/";
 static const NSString *apiUsername = @"moments";
 static const NSString *apiPassword = @"qHCLgGKUcKGcEf8avrKRr9JqygeohXJZ";
 
